6bd531bb23464548a095c8f73c0dd1728621b9e8
Paulouss <paulovinicius.pvlf@gmail.com>
2025-05-06 22:59:10 -0300

    fluxo-vital-hospital

---
+++ java script hospital.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('#cadastro form');
+
+  if (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+
+      
+      const nome = form.querySelector('input[type="text"]').value.trim();
+      const email = form.querySelector('input[type="email"]').value.trim();
+
+    
+      if (nome && email) {
+        alert(`Obrigado, ${nome}! Seu cadastro foi enviado com sucesso para o e-mail ${email}.`);
+        form.reset();
+      } else {
+        alert("Por favor, preencha seu nome e e-mail antes de enviar.");
+      }
+    });
+  }
+});
+
+
